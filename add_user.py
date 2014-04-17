6c49cdaa27ad5a8c541cb3667a8f70a5eab0fb09
David Gilman <davidgilman1@gmail.com>
2014-04-16 21:27:42 -0500

    add_user: create users with download flag set

--- add_user.py
+++ add_user.py
@@ -9,7 +9,7 @@ for line in sys.stdin:
       break
    user, pw = line.strip().split('|')
    hashed = bcrypt.hashpw(pw, bcrypt.gensalt())
-   conn.execute('INSERT INTO users (id, username, password) VALUES (NULL, ?, ?)', (user, hashed))
+   conn.execute('INSERT INTO users (id, username, password, download) VALUES (NULL, ?, ?, 1)', (user, hashed))
 
 conn.commit()
 
